ad1d6292dd79484593b835e489b7177c864810aa
Joel Lebron <email": null>
2017-04-12 14:21:11 -0400

    Added domain filter to validurl

--- js/smokescreen.js
+++ js/smokescreen.js
@@ -51,44 +51,41 @@ function saveUrls(url) {
   browser.storage.local.set({
     myFeed: myFeed
   })
-  //console.log("my feed: " + myFeed);
 }
 
-function isLoop(url){
-  // console.log("is-loop url: " + url);
-  // console.log("url type: " + typeof(url));
-  // console.log("url in myFeed? " + myFeed.includes(url));
-  if(myFeed.includes(url)){
-    console.log("caught in loop");
-    browser.runtime.sendMessage("restart");
-    return true;
-  }
-  else{
-    return false;
-  }
+function validDomain(url){
+  var regmatch = new Array(
+    /[.]com/i,/[.]org/i,/[.]net/i,/[.]edu/i,/[.]gov/i,/[.]mil/i,/[.]boston/i,/[.]miami/i,/[.]nyc/i,/[.]quebec/i,/[.]vegas/i,);
+  for(regex of regmatch){
+    if(regex.test(url)){
+      console.log("proper domain found " + url);
+
+      return true;
+    };
+
+  };
+
+  console.log("Improper domain found");
+  return false;
 }
 
 function validUrl(url){
   console.log(url);
   var regskip = new Array(
-  /calendar/i,/advanced/i,/click/i,/Groups/i,/Images/,/Maps/,/search/i,/cache/i
-    ,/similar/i,/&#169;/,/signup/i,/help[^Ss]/i,/download/i,/print/i,/Books/i,/rss/i
+  /calendar/i,/advanced/i,/click/i,/Groups/i,/Images/,/Maps/i,/search/i,/cache/i
+    ,/similar/i,/&#169;/,/signup/i,/download/i,/print/i,/Books/i,/rss/i
     ,/xRank/,/permalink/i,/aggregator/i,/trackback/i,/comment/i,/More/
     ,/business solutions/i,/register/i,/result/i,/view/i,/Legal/,/See all/,/links/i,/submit/i
-    ,/Sites/i,/ click/i,/Blogs/,/See your mess/,/feedback/i,/sponsored/i,/preferences/i
+    ,/Sites/i,/ click/i,/Blogs/,/feedback/i,/sponsored/i,/preferences/i
     ,/privacy/i,/News/,/Finance/,/Reader/,/Documents/,/windows live/i,/tell us/i
     ,/shopping/i,/Photos/,/Video/,/Scholar/,/AOL/,/advertis/i,/Webmasters/,/MapQuest/
     ,/Movies/,/Music/,/Yellow Pages/,/jobs/i,/answers/i,/options/i,/customize/i,/settings/i
-    ,/Developers/,/cashback/,/Health/,/Products/,/QnABeta/,/<more>/,/Travel/,/Personals/
+    ,/Developers/,/cashback/,/Health/,/Products/,/<more>/,/Travel/,/Personals/
     ,/Local/,/Trademarks/,/cache/i,/similar/i,/login/i,/signin/i,/mail/i,/feed/i,/pay/i
     ,/accounts/i,/[.]tar$/,/[.]exe$/,/[.]zip$/,/[.]pdf$/,/[.]wav$/,/[.]txt$/,/[.]js$/
     ,/[.]jse$/,/[.]msi$/,/[.]bat$/,/[.]reg$/,/[.]doc$/,/[.]xls$/,/[.]ppt$/,/[.]gz$/,/[.]tgz$/);
 
 
-  if(url == "https://www.58pic2017.org/"){
-    console.log("Caught the trap!");
-    return false;
-  }
   for(regex of regskip){
     if(regex.test(url)){
         console.log(regex + ". skipping " + url);
@@ -96,7 +93,9 @@ function validUrl(url){
         return false;
       };
       //one more test with testing for url in loop
-      //isLoop(url);
+      if( !validDomain(url)){
+        return false;
+      }
     };
   return true;
 
